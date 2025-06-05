f6c5ecd9bd5a42d3900fd0c81e8c1798fae62bd6
Divyanshi <divyanshitalreja12@gmail.com>
2025-06-05 23:13:16 +0530

    Removed the deprecated UTC function

--- to-do-l1st/app/models.py
+++ to-do-l1st/app/models.py
@@ -1,11 +1,11 @@
 from app import db
-from datetime import datetime
+from datetime import datetime, UTC
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
-    date = db.Column(db.Date, default=datetime.utcnow)
+    date = db.Column(db.Date, default=datetime.now(UTC))
     
     def __repr__(self):
         return self.text
